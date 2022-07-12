f3a8354751693220965b0cbdfe4c68f1bdefe2f8
anreh <anurehka@gmail.com>
2022-07-12 10:58:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImg: ImageView
+    lateinit var numberText: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        diceImg = findViewById(R.id.diceImg)
+        numberText = findViewById(R.id.numberText)
+        //val rollButton: Button = findViewById(R.id.rollButton)
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        rollButton.setOnClickListener{rolldice()}
+    }
+
+    private fun rolldice(){
+        val randomNumb = (1..6) . random ()
+        //val numberText: TextView = findViewById(R.id.numberText)
+        //val diceImg: ImageView = findViewById(R.id.diceImg)
+        numberText.text = randomNumb. toString()
+        val imgSrc = when (randomNumb){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImg.setImageResource(imgSrc)
+
+        Toast.makeText(this, randomNumb.toString(),
+        Toast. LENGTH_SHORT).show()
+}
+
+}
