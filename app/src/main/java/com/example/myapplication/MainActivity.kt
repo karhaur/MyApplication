6d7060dae72df06d2019db259530b243e2eac987
anreh <anurehka@gmail.com>
2022-07-19 10:30:27 +0800

    Commit 20221907 1029 - Added user interactivity - EditText Added - Update button added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,32 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImg: ImageView
     lateinit var numberText: TextView
+    lateinit var editPlayerName: EditText
+    lateinit var playerNameTV: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         diceImg = findViewById(R.id.diceImg)
         numberText = findViewById(R.id.numberText)
+        editPlayerName = findViewById(R.id.editPlayerName)
+        playerNameTV = findViewById(R.id.playerName)
         //val rollButton: Button = findViewById(R.id.rollButton)
         val rollButton = findViewById<Button>(R.id.rollButton)
-        rollButton.setOnClickListener{rolldice()}
+        rollButton.setOnClickListener{rollDice()}
+        val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)
+        updateButton.setOnClickListener{updatePlayerName(it)}
     }
 
-    private fun rolldice(){
+    private fun rollDice(){
         val randomNumb = (1..6) . random ()
         //val numberText: TextView = findViewById(R.id.numberText)
         //val diceImg: ImageView = findViewById(R.id.diceImg)
@@ -38,5 +44,13 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, randomNumb.toString(),
         Toast. LENGTH_SHORT).show()
 }
+    private fun updatePlayerName(view: View){
+        playerNameTV.text = editPlayerName.text
 
+        editPlayerName.text.clear()
+
+        //HIDE KEYBOARD AFTER UPDATE NAME
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
